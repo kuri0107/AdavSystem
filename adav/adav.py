b2dc55fe9281619f9cfdb603c6a2c7b2a9c88c05
goto <goto.11141997@gmail.com>
2018-12-10 15:36:21 +0900

    アップロードのアップロード

--- adav/adav.py
+++ adav/adav.py
@@ -1,8 +1,9 @@
 from flask import render_template, Flask,request,Response
-import json,base64
-
+import json,base64,datetime
 app = Flask(__name__)
 
+FILE_FORMAT = ".jpeg"
+FILE_PATH = "static/images/"
 #トップページへの遷移
 @app.route('/')
 def top():
@@ -20,18 +21,24 @@ def capture():
     getdata = request.data
 
     #TODO 分析した結果異常があった場合サーバに画像を保存
-    #TODO ファイルする保存名を日時にする
-    filename = ""
-    with open("static/images/input.jpeg", "wb") as f:
+
+    #ファイル名を日時にする
+    date = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
+    print("日付型" + date)
+    filename = str(date) + FILE_FORMAT
+    print("ファイル名" + filename)
+
+    with open(FILE_PATH + filename, "wb") as f:
         f.write(base64.decodestring(getdata))
 
+    #TODO 分析した結果異常なデータを返す
+
+
+    #取得したバイナリを文字列型に変換
     #先端のb'と終端の'を取り除いて返す
     #str(getdata) → b'XXXX...X'
     #str(getdata)[2:len(str(getdata))-1] → XXXX...X
     retdata = [str(getdata)[2:len(str(getdata))-1]]
-
-    #TODO 分析した結果異常なデータを返す
-
     #base64のバイナリデータをjson形式でレスポンスとして返す
     return Response(json.dumps(retdata))
 
