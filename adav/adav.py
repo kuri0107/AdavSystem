92ab4cc1289db27af78812f74002158cf4fbece3
goto <goto.11141997@gmail.com>
2018-12-11 09:50:19 +0900

    キャプチャした画像をcaptureフォルダに保存可能

--- adav/adav.py
+++ adav/adav.py
@@ -6,51 +6,56 @@ app = Flask(__name__)
 FILE_FORMAT_PNG = ".png"
 FILE_FORMAT_JPEG = ".jpeg"
 
-# 保存先のパス
-FILE_PATH = "static/images/capture"
+# JPEGイメージヘッダ部の終端座標
+HEADER_IDX = 23
+START_BYTE_IDX = 2
+END_BYTE_IDX = 1
 
-#トップページへの遷移
+
+# 保存先のパス
+FILE_PATH = "static/capture/"
+
+# トップページへの遷移
 @app.route('/')
 def top():
     return render_template('top.html')
 
-#メインページへの遷移
+# メインページへの遷移
 @app.route('/main', methods=['GET'])
 def display():
     return render_template('main.html')
 
-#ajax通信を用いてキャプチャした画像を送信し分析した結果を返す
+# ajax通信を用いてキャプチャした画像を送信し分析した結果を返す
 @app.route('/capture',methods=['POST'])
 def capture():
-    #画像データのリクエスト
+    # 画像データのリクエスト
     getdata = request.data
 
-    #TODO 分析した結果異常があった場合サーバに画像を保存
 
-    #ファイル名を日時にする
+    #TODO 分析した結果、異常があった場合サーバに画像を保存
+
+    # captureフォルダに保存
+    # ファイル名を日時にする
     date = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
     # print("日付型" + date)
 
-    #JPEG
-    #filename = str(date) + FILE_FORMAT_JPEG
-
-    #PNG
-    filename = str(date) + FILE_FORMAT_PNG
+    # JPEG
+    filename = str(date) + FILE_FORMAT_JPEG
+    # PNG
+    # filename = str(date) + FILE_FORMAT_PNG
 
     # print("ファイル名" + filename)
 
-
-    # with open(FILE_PATH + filename, "wb") as f:
-    #      f.write(base64.decodestring(getdata))
-
     #TODO 分析した結果異常なデータを返す
 
+    with open(FILE_PATH + filename, "wb") as f:
+         f.write(base64.decodestring(getdata[HEADER_IDX:]))
 
     #取得したバイナリを文字列型に変換
     #先端のb'と終端の'を取り除いて返す
     #str(getdata) → b'XXXX...X'
     #str(getdata)[2:len(str(getdata))-1] → XXXX...X
-    retdata = [str(getdata)[2:len(str(getdata))-1]]
+    retdata = [str(getdata)[START_BYTE_IDX:len(str(getdata))-END_BYTE_IDX]]
     #base64のバイナリデータをjson形式でレスポンスとして返す
     return Response(json.dumps(retdata))
 
